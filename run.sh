151908a03b76ab90f87e021189bf5fe3f21548d5
kyungpyo-kim <kyungpyo94@gmail.com>
2020-05-25 22:49:07 +0900

    init

---
+++ run.sh
@@ -0,0 +1,6 @@
+# for training
+python3 main.py --load_config=fcn8s_shufflenet_train.yaml train Train FCN8sShuffleNet
+
+# for testing with validation data
+python3 main.py --load_config=fcn8s_shufflenet_test.yaml test_eval Train FCN8sShuffleNet
+
